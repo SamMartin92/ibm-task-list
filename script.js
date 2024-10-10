28bca9e5d4b6c22a319bdeb33d6566e408cd1023
Sam <thesammartin1@gmail.com>
2024-10-10 23:08:31 +0100

    move all files to repo in one big commit

---
+++ script.js
@@ -0,0 +1,73 @@
+
+//constants declared for input button and task list area
+const taskInput = document.querySelector("#new-task-input");
+const dateInput = document.querySelector("#due-date");
+const taskSection = document.querySelector(".tasks");
+
+//array of tasks
+const listOfTasks = [];
+
+//listener for Enter key. Used to add new task
+taskInput.addEventListener("keyup", (e) => {
+    if (e.key == "Enter" && dateInput.value != "") {
+        createTask();
+    } 
+});
+
+//onClick event for add button
+document.querySelector('#push').onclick = function() {
+    createTask();
+}
+//function that creates task
+function createTask(){
+    const date = new Date();
+    const dueDate = new Date(dateInput.value);
+
+    if (taskInput.value.length == 0){
+        alert("This field is blank. Enter a task name and try again");
+    } 
+    else if (dueDate< date.setHours(0,0,0)){
+        alert("Due date cannot be in the past.");
+    } 
+    else {
+        taskSection.innerHTML +=
+        `<div class="task">
+        <label id="taskname">
+        <input onclick="updateTask(this)" type="checkbox" id="check-task">
+        <p>${taskInput.value}</p>
+        </label>
+        <span>Due: ${dateInput.value}</span>
+        <div class="delete">
+        <i class="uil uil-trash"></i></div></div>
+        `
+
+        listOfTasks.push({
+            'taskName': taskInput.value,
+            'taskdate': dueDate
+        })
+
+        taskInput.value='';
+        dateInput.value='';
+
+        console.log(listOfTasks);
+    }
+    var currentTasks = document.querySelectorAll(".delete");
+    for (var i =0; i <currentTasks.length; i++){
+        currentTasks[i].onclick = function() {
+            this.parentNode.remove();
+        }
+    }
+
+    taskSection.offsetHeight>=300
+    ? taskSection.classList.add("overflow")
+    : taskSection.classList.remove("overflow")
+}
+
+function updateTask(task){
+    let taskItem = task.parentElement;
+    if (task.checked){
+        taskItem.classList.add("checked");
+    } else {
+        taskItem.classList.remove("checked");
+    }
+}
